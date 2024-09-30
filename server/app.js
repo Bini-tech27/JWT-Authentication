ab3b4a45acb574ea60af83ef39593e807a873af0
Bini <binilulseged@gmail.com>
2024-09-30 08:18:12 +0300

    updated

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const mysql = require("mysql2");
-const PORT = 5050;
 
 const app = express();
 
 app.use(express.json());
+
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
     console.log(`"Server is running at port ${PORT}"`)
