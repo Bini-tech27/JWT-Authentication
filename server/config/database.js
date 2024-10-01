e957fc4b84802fa42a13f683690838ceb7ad32ad
Bini <binilulseged@gmail.com>
2024-10-01 16:48:37 +0300

    updated

--- server/config/database.js
+++ server/config/database.js
@@ -17,3 +17,29 @@ AppDataSource.initialize()
     .catch((err) => {
         console.error("Error during Data Source initialization", err)
     })
+
+
+    // module.exports = {
+//     type: "mysql",
+//     host: process.env.DB_HOST,
+//     port: 3306,
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     synchronize: true, // Automatically create tables based on entities (use in development only)
+//     logging: true,
+//     entities: [
+//         "src/entity/**/*.js"
+//     ],
+//     migrations: [
+//         "src/migration/**/*.js"
+//     ],
+//     subscribers: [
+//         "src/subscriber/**/*.js"
+//     ],
+//     cli: {
+//         entitiesDir: "src/entity",
+//         migrationsDir: "src/migration",
+//         subscribersDir: "src/subscriber"
+//     }
+// };
--- server/config/db.js
+++ server/config/db.js
@@ -15,28 +15,5 @@ module.exports = connection;
 
 
 
-// module.exports = {
-//     type: "mysql",
-//     host: process.env.DB_HOST,
-//     port: 3306,
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     synchronize: true, // Automatically create tables based on entities (use in development only)
-//     logging: true,
-//     entities: [
-//         "src/entity/**/*.js"
-//     ],
-//     migrations: [
-//         "src/migration/**/*.js"
-//     ],
-//     subscribers: [
-//         "src/subscriber/**/*.js"
-//     ],
-//     cli: {
-//         entitiesDir: "src/entity",
-//         migrationsDir: "src/migration",
-//         subscribersDir: "src/subscriber"
-//     }
-// };
+
 
