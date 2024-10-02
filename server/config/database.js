5781f4c985a9685c18adc8e18de7c21e1c0a8cfb
Bini <binilulseged@gmail.com>
2024-10-02 10:27:44 +0300

    updated

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,5 @@
 import { DataSource } from "typeorm"
+import { User } from "../models/User"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -19,28 +20,4 @@ AppDataSource.initialize()
         console.error("Error during Data Source initialization", err)
     })
 
-
-    // module.exports = {
-//     type: "mysql",
-//     host: process.env.DB_HOST,
-//     port: 3306,
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     synchronize: true, // Automatically create tables based on entities (use in development only)
-//     logging: true,
-//     entities: [
-//         "src/entity/**/*.js"
-//     ],
-//     migrations: [
-//         "src/migration/**/*.js"
-//     ],
-//     subscribers: [
-//         "src/subscriber/**/*.js"
-//     ],
-//     cli: {
-//         entitiesDir: "src/entity",
-//         migrationsDir: "src/migration",
-//         subscribersDir: "src/subscriber"
-//     }
-// };
+module.exports = AppDataSource;
